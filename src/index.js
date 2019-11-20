609106a2c0da818db00ec6c1ff99fcd44e207d2e
yc_tao <429295712@qq.com>
2019-11-20 14:25:30 +0800

    save

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const testPromise = new Promise((resolve, reject) => {
   resolve();
 });
 exports.showMsg = function () {
-  const test = "wfa's second module";
+  const test = "wfa's second module111";
   const test1 = target;
   console.log(test + ' ' + test1);
 };
