1291e57851fe57a282730e06043f921a2fe8e9ce
yc_tao <429295712@qq.com>
2019-11-20 14:38:29 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-require('babel-polyfill');
-const target = require('./util/tool')
-console.log(target);
-// const testPromise = Promise.resolve();
-const testPromise = new Promise((resolve, reject) => {
-  resolve();
-});
 exports.showMsg = function () {
-  const test = "wfa's second module111";
-  const test1 = target;
-  console.log(test + ' ' + test1);
+  const test = "wfa's second module 4.0.2";
+  console.log(test);
 };
--- src/util/tool.js
+++
@@ -1,2 +0,0 @@
-const test = 2;
-exports.test1 = test;
