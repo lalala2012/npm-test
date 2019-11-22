c1839021aa48cd39c30fccb5f9b4dd1d279033f6
yc_tao <429295712@qq.com>
2019-11-22 16:28:32 +0800

    feat:添加入口的绝对路径设定

--- build/webpack.conf.base.js
+++ build/webpack.conf.base.js
@@ -3,15 +3,21 @@ function resolve(dir) {
   return path.join(__dirname, '..', dir);
 }
 module.exports = {
-  // entry: '../src/components/index.js',
-  // entry: '../src/index.js',
   // 文件只能一个个导入，最好有一个入口文件
-  entry: path.resolve(__dirname, 'entryTest/main'),
+  // entry: path.resolve(__dirname, '../build/entryTest/main.js'),
+  // entry: path.resolve(__dirname, '.entryTest/main.js'),
+  // 有无后缀都行，之人文件名，后面loader在自行过滤
+  // entry: path.resolve(__dirname, '../src/ma2in.js'),
+  // 下面几个配合着用
+  // 设定入口的绝对路径
+  context: path.resolve(__dirname, '../'),
+  // entry: './src/index.js',
+  entry: './build/entryTest/main.js',
   // 配置输入文件
   output: {
     // path: path.resolve(__dirname, 'dist',),
     // 文件之间支持使用../ 返回上一级
-    path: path.resolve(__dirname, '../dist',),
+    path: path.resolve(__dirname, '../dist'),
     // 当前绝对路径
     filename: 'bundle.js'
     // 配置输出文件
