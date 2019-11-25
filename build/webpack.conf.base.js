4f499256914fd875e5ac2e71ce2143c70e73dc39
yc_tao <429295712@qq.com>
2019-11-25 21:43:24 +0800

    feat:save

--- build/webpack.conf.base.js
+++ build/webpack.conf.base.js
@@ -1,29 +1,12 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 module.exports = {
-  // 文件只能一个个导入，最好有一个入口文件
-  // entry: path.resolve(__dirname, '../build/entryTest/main.js'),
-  // entry: path.resolve(__dirname, '.entryTest/main.js'),
-  // 有无后缀都行，之人文件名，后面loader在自行过滤
-  // entry: path.resolve(__dirname, '../src/index.js'),
   // 下面几个配合着用
   // 设定入口的绝对路径
   context: path.resolve(__dirname, '../'),
-  // entry: './src/index.js',
-  entry: './src/components/index.js',
-  // entry: './build/entryTest/main.js',
-  // entry: './src/components/downtime/index.vue',
-  // entry: './src/components/test/index.vue',
+  entry: './src/index.js',
   // 配置输入文件
   output: {
-    // path: path.resolve(__dirname, 'dist',),
-    // 文件之间支持使用../ 返回上一级
-    // path: path.resolve(__dirname, '../dist'),
-    // 当前绝对路径
-    // filename: 'bundle.js'
-    // 配置输出文件
-    // 这里配置好了不用在命令行再写输入输出名
-    // 输出设定根路径
     path: path.resolve(__dirname, '../dist'),
     filename: '[name].js',
     libraryTarget: 'umd',
@@ -33,12 +16,6 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: 'vue-loader',
-        options: {
-          loaders: {
-            scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
-            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax' // <style lang="sass">
-          }
-        }
       },
       // 它会应用到普通的 `.js` 文件
       // 以及 `.vue` 文件中的 `<script>` 块
